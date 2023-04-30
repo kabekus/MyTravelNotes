f946832632a7df1870db8f6260635c4822c171c2
kabekus <kabekus.123@gmail.com>
2023-04-30 20:36:08 +0300

    Permission Controls

--- app/src/main/java/com/kabekus/mytravelnotes/DetailActivity.kt
+++ app/src/main/java/com/kabekus/mytravelnotes/DetailActivity.kt
@@ -1,11 +1,42 @@
 package com.kabekus.mytravelnotes
 
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.view.View
+import android.view.inputmethod.InputBinding
+import androidx.activity.result.ActivityResultLauncher
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import com.google.android.material.snackbar.Snackbar
+import com.kabekus.mytravelnotes.databinding.ActivityDetailBinding
 
 class DetailActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailBinding
+    private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
+    private lateinit var permissionLauncher : ActivityResultLauncher<String>
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+    fun saveButtonClicked(view : View){
+
+    }
+    fun selectImage(view: View){
+        if(ContextCompat.checkSelfPermission(this,Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION) != PackageManager.PERMISSION_GRANTED ){
+
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)){
+                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE)
+                    .setAction("Give Permission",View.OnClickListener {
+
+                }).show()
+            }
+        }else{
+            val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        }
     }
 }
--- app/src/main/java/com/kabekus/mytravelnotes/MainActivity.kt
+++ app/src/main/java/com/kabekus/mytravelnotes/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.kabekus.mytravelnotes
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import com.kabekus.mytravelnotes.databinding.ActivityDetailBinding
+import com.kabekus.mytravelnotes.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+    }
+
+    //Bağlama işlemi
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val menuInflater = menuInflater
+        menuInflater.inflate(R.menu.menu,menu)
+        return super.onCreateOptionsMenu(menu)
+
+    }
+
+    //Tıklandığında yapılacak işlemler
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.addItem){
+            val intent = Intent(this@MainActivity,DetailActivity::class.java)
+            startActivity(intent)
+        }
+
+        return super.onOptionsItemSelected(item)
     }
 }
