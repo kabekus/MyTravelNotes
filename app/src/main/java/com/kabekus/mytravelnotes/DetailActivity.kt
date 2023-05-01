5e4754178ce6f2f4fc390dbf2fb38c661dc82acd
kabekus <kabekus.123@gmail.com>
2023-05-01 16:20:55 +0300

    This commit includes use permissions in the project

--- app/src/main/java/com/kabekus/mytravelnotes/DetailActivity.kt
+++ app/src/main/java/com/kabekus/mytravelnotes/DetailActivity.kt
@@ -1,42 +1,112 @@
 package com.kabekus.mytravelnotes
 
+import android.Manifest.permission.READ_EXTERNAL_STORAGE
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.os.Build
+import android.os.Build.VERSION_CODES.TIRAMISU
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
 import android.view.View
-import android.view.inputmethod.InputBinding
+import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.google.android.material.snackbar.Snackbar
 import com.kabekus.mytravelnotes.databinding.ActivityDetailBinding
+import android.Manifest
 
 class DetailActivity : AppCompatActivity() {
+    var selectedBitmap : Bitmap? = null
     private lateinit var binding: ActivityDetailBinding
     private lateinit var activityResultLauncher : ActivityResultLauncher<Intent>
     private lateinit var permissionLauncher : ActivityResultLauncher<String>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityDetailBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        registerLauncher()
     }
     fun saveButtonClicked(view : View){
 
     }
     fun selectImage(view: View){
-        if(ContextCompat.checkSelfPermission(this,Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION) != PackageManager.PERMISSION_GRANTED ){
+        if(Build.VERSION.SDK_INT >= TIRAMISU){
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED ){
 
-            if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.DYNAMIC_RECEIVER_NOT_EXPORTED_PERMISSION)){
-                Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE)
-                    .setAction("Give Permission",View.OnClickListener {
-
-                }).show()
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_MEDIA_IMAGES)){
+                    Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission",View.OnClickListener {
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                        }).show()
+                }else{
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES)
+                }
+            }else{
+                val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
             }
         }else{
-            val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+            if(ContextCompat.checkSelfPermission(this,Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED ){
+
+                if (ActivityCompat.shouldShowRequestPermissionRationale(this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    Snackbar.make(view,"Permission needed for gallery",Snackbar.LENGTH_INDEFINITE)
+                        .setAction("Give Permission",View.OnClickListener {
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                        }).show()
+                }else{
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+                }
+            }else{
+                val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
+            }
+        }
+
+    }
+
+    private fun registerLauncher(){
+        activityResultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result ->
+
+            if (result.resultCode == RESULT_OK){
+                 val intentFromResult = result.data
+                if (intentFromResult != null){
+                    val imageData = intent.data
+                    //binding.imageView.setImageURI(imageData)
+                    if (imageData != null){
+                        try {
+                            if (Build.VERSION.SDK_INT >= 28) { // Min SDK 28 gerektiriyor onun kontrolü
+                                val source = ImageDecoder.createSource(this@DetailActivity.contentResolver, imageData)
+                                selectedBitmap = ImageDecoder.decodeBitmap(source)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }else{
+                                selectedBitmap = MediaStore.Images.Media.getBitmap(contentResolver,imageData)
+                                binding.imageView.setImageBitmap(selectedBitmap)
+                            }
+                        }catch (e: Exception){
+                            e.printStackTrace()
+                        }
+                    }
+
+                }
+            }
+        }
+
+        permissionLauncher = registerForActivityResult(ActivityResultContracts.RequestPermission()){ result ->
+            if (result){
+                val intentToGalery = Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                activityResultLauncher.launch(intentToGalery)
+            }else{
+                Toast.makeText(this@DetailActivity,"Permission Needed !",Toast.LENGTH_LONG).show()
+            }
+
         }
     }
 }
